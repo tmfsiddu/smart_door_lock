211c89df39cf4915ec6e288a957d500ac124878c
parthu1029 <parthasaradhiganta@gmail.com>
2026-03-04 00:59:26 +0530

    fix starting

--- server.js
+++ server.js
@@ -26,8 +26,4 @@ app.post("/verify-otp", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000; 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;

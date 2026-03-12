b954bd26cd1ae583c955f0b0fff184906704b13d
Your Name <you@example.com>
2026-03-12 21:56:05 +0530

    made minor changes

--- src/server.js
+++ src/server.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 // ===== Temporary OTP Storage =====
 let currentOTP = null;
 let otpExpiry = null;
 
-// ===== Serve Static Website =====
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
